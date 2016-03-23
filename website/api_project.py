ec35a8adf9c376fa30053d3afea43af516d2024f
Trish Whetzel <plwhetzel@gmail.com>
2016-03-23 15:23:19 -0700

    update to use form field value as input to profiling script

--- website/api_project.py
+++ website/api_project.py
@@ -10,6 +10,8 @@ import requests
 from flask import make_response
 from datetime import datetime
 
+from scripts import profiling
+
 
 app = Flask(__name__)
 # Use SeaSurf to prevent cross-site request forgery
@@ -17,24 +19,22 @@ csrf = SeaSurf(app)
 
 
 # Home page for smartAPI Web service annotation
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 @app.route('/home', methods=['GET', 'POST'])
 def show_home():
 	if request.method == 'POST':
 		ws_input = request.form['ws_input']
 		app.logger.info(ws_input)
-		profiling.py
-		#return redirect(url_for('show_annotations'))
-		return render_template('annotation_results.html', ws_input=ws_input)
+		# api_calls_to_profile = getattr(profiling, 'get_calls')()
+		# app.logger.info(api_calls_to_profile)
+		# demo_output = getattr(profiling, 'build_api_profile')(api_calls_to_profile)
+		# #app.logger.info(demo_output)
+		demo_output = profiling.main(ws_input)
+		return render_template('annotation_results.html', ws_input=ws_input, \
+			demo_output=demo_output)
 	else:
-		app.logger.info('** Showing Home page')
+		app.logger.info('** Showing Home page **')
 		return render_template('index.html')
-
-
-# Show results of automatic annotation
-# @app.route('/results/', methods=['GET', 'POST'])
-# def show_annotations():
-# 	return render_template('annotation_results.html')
 
 
 if __name__ == '__main__':
