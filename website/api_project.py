5ace649cb8c069a62cfa124b3cb2d0f171d7ae6f
Trish Whetzel <plwhetzel@gmail.com>
2016-03-30 17:26:06 -0700

    change to post 5000

--- website/api_project.py
+++ website/api_project.py
@@ -40,4 +40,4 @@ def show_home():
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=5000)
