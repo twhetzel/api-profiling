8669e6840bd28bcfd7d168c026a0cee40905cef0
Trish Whetzel <plwhetzel@gmail.com>
2016-05-03 22:49:19 -0700

    add method to generate category autocomplete data from miriam data

--- website/scripts/miriam_datatype_identifiers.py
+++ website/scripts/miriam_datatype_identifiers.py
@@ -43,8 +43,28 @@ def build_miriam_name_dictionary(miriam_datatype_obj):
     return miriam_name_dict
 
 
+# Build MIRIAM ID / Name Autocomplete data structure
+# E.g. [{value: 'MIR:00000001', label: 'Wikipedia'}, {value: 'MIR:0000007', label: 'Some Resource'}]
+def build_miriam_autocomplete_data(miriam_datatype_obj):
+    #print miriam_datatype_obj
+    autocomplete_data = []
+
+    for root, value_obj in miriam_datatype_obj.iteritems():
+        for k_v_obj in value_obj:
+            autocomplete_obj = {}
+            autocomplete_obj['value'] = str(k_v_obj['id'])
+            autocomplete_obj['label'] = str(k_v_obj['name'])
+            autocomplete_obj['category'] = ''
+            autocomplete_data.append(autocomplete_obj)
+            print autocomplete_data
+
+    return autocomplete_data
+
+
 # Main Program
 if __name__ == '__main__':
     miriam_datatype_obj = get_miriam_datatypes()
     miriam_datatype_dict = build_miriam_identifier_dictionary(miriam_datatype_obj)
-    print "Test: ", miriam_datatype_dict['Yeast Intron Database v4.3']
+    #print "Test: ", miriam_datatype_dict['Wikidata']
+
+    miriam_autocomplete_data = build_miriam_autocomplete_data(miriam_datatype_obj)
