221dae60c04891e8d53c0db31ce2a6430342e7ea
Trish Whetzel <plwhetzel@gmail.com>
2017-01-20 10:51:22 -0800

    clean up comments and PEP8 compliance

--- website/api_project.py
+++ website/api_project.py
@@ -4,23 +4,12 @@ from __future__ import unicode_literals
 from __future__ import absolute_import
 from future.utils import iteritems
 
-from flask import Flask, render_template, request, redirect, jsonify, \
-    url_for, flash, Response
-import random
-import string
+from flask import Flask, render_template, request
 import logging
-import json
-import httplib2
-import requests
-from flask import make_response
-from datetime import datetime
 
-from scripts import profiling, miriam_datatype_identifiers, test_patterns, data_registry_urls
+from scripts import profiling, miriam_datatype_identifiers, \
+    test_patterns, data_registry_urls
 import re
-import copy
-import ast
-from collections import deque
-import collections
 
 
 app = Flask(__name__)
@@ -30,42 +19,42 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/home', methods=['GET', 'POST'])
 def show_home():
-	if request.method == 'POST' and request.form['ws_input']:
-		ws_input = request.form['ws_input']
-		app.logger.info(ws_input)
-		# api_calls_to_profile = getattr(profiling, 'get_calls')()
-		# app.logger.info(api_calls_to_profile)
-		# demo_output = getattr(profiling, 'build_api_profile')(api_calls_to_profile)
-		# #app.logger.info(demo_output)
-		global demo_output
-		demo_output = profiling.main(ws_input)
+    if request.method == 'POST' and request.form['ws_input']:
+        ws_input = request.form['ws_input']
+        app.logger.info('** Web service input: '+ws_input)
+        global demo_output
+        demo_output = profiling.main(ws_input)
 
-		# Get master_identifier_dictionary for value display in result page
-		api_calls = profiling.get_calls_from_form(ws_input)
-		master_id_dictionary = profiling.build_api_profile(api_calls)
+        # Get master_identifier_dictionary for value display in result page
+        api_calls = profiling.get_calls_from_form(ws_input)
+        master_id_dictionary = profiling.build_api_profile(api_calls)
 
-		# Build dictionary of MIRIAM datatypes
-		miriam_datatype_obj = miriam_datatype_identifiers.get_miriam_datatypes()
-		miriam_name_dict = miriam_datatype_identifiers.build_miriam_name_dictionary(miriam_datatype_obj)
+        # Build dictionary of MIRIAM datatypes
+        miriam_datatype_obj = miriam_datatype_identifiers.get_miriam_datatypes()  # noqa
+        miriam_name_dict = miriam_datatype_identifiers.build_miriam_name_dictionary(miriam_datatype_obj)  # noqa
 
-		# Get regex pattern data
-		all_pattern_data = test_patterns.get_all_pattern_data()
-		all_pattern_dict = test_patterns.make_pattern_dictionary(all_pattern_data)
+        # Get regex pattern data
+        all_pattern_data = test_patterns.get_all_pattern_data()
+        all_pattern_dict = test_patterns.make_pattern_dictionary(all_pattern_data)  # noqa
 
-		# Get dictionary of MIRIAM Ids and namespace URLs
-		data_registry_id_url_dict = data_registry_urls.build_miriam_url_dictionary()
+        # Get dictionary of MIRIAM Ids and namespace URLs
+        data_registry_id_url_dict = data_registry_urls.build_miriam_url_dictionary()  # noqa
 
-		return render_template('annotation_results.html', ws_input=ws_input, \
-			demo_output=demo_output, master_id_dictionary=master_id_dictionary, \
-			miriam_name_dict=miriam_name_dict, \
-			all_pattern_dict=all_pattern_dict, re=re, iteritems=iteritems,
-			data_registry_id_url_dict=data_registry_id_url_dict)
-	else:
-		app.logger.info('** Showing Home page **')
-		return render_template('index.html')
+        return render_template(
+            'annotation_results.html',
+            ws_input=ws_input,
+            demo_output=demo_output,
+            master_id_dictionary=master_id_dictionary,
+            miriam_name_dict=miriam_name_dict,
+            all_pattern_dict=all_pattern_dict,
+            re=re,
+            iteritems=iteritems,
+            data_registry_id_url_dict=data_registry_id_url_dict)
+    else:
+        app.logger.info('** Display Home page **')
+        return render_template('index.html')
 
 
-# From Hello World Sample, https://github.com/GoogleCloudPlatform/python-docs-samples/blob/master/appengine/flexible/hello_world/main.py
 @app.errorhandler(500)
 def server_error(e):
     logging.exception('An error occurred during a request.')
