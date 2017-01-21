ffac2fa76dd7d01688127b22bb13be695e3bc77f
Trish Whetzel <plwhetzel@gmail.com>
2017-01-20 16:29:56 -0800

    update formatting for PEP8

--- website/scripts/data_registry_urls.py
+++ website/scripts/data_registry_urls.py
@@ -1,15 +1,13 @@
 import json
 
-# Build dictionary of ID/URL from 
-# full Identifiers.org resource information
 
-# Build Identifier->URL dictionary
 def build_miriam_url_dictionary():
+    """ Build dictionary mapping MIRIAM ID to URL
+    from full Identifiers.org resource information. """
     with open('./data/data_registry.json') as data_file:
         data = json.load(data_file)
-    
+
     url_dict = {}
-    # Iterate through list of resources
     for key in data:
         url_key = key["id"]
         url_value = key["url"]
@@ -20,4 +18,3 @@ def build_miriam_url_dictionary():
 # Main Program
 if __name__ == '__main__':
     data_registry_url_dict = build_miriam_url_dictionary()
-
