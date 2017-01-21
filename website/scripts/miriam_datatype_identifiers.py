a40a5ebe52392b95d372e312915ccde8d18e4349
Trish Whetzel <plwhetzel@gmail.com>
2017-01-21 10:55:41 -0800

    update formatting for PEP8

--- website/scripts/miriam_datatype_identifiers.py
+++ website/scripts/miriam_datatype_identifiers.py
@@ -2,20 +2,17 @@
 from __future__ import print_function
 from future.utils import iteritems
 
-import json
-import requests
-
-from collections import deque
+import json  # noqa
+import requests  # noqa
 from collections import OrderedDict
 from operator import itemgetter
 
 
-# Get MIRIAM Identifiers for Resources/Datatypes 
 def get_miriam_datatypes():
+    """ Get MIRIAM Identifiers for Resources. """
     miriamws = "http://www.ebi.ac.uk/miriamws/main/rest/datatypes/"
+    r = requests.get(miriamws, headers={"Accept": "application/json"})
 
-    r = requests.get(miriamws, headers={"Accept":"application/json"})
-    
     if r.status_code == 200:
         response = r.text
         data = json.loads(response)
@@ -27,46 +24,48 @@ def get_miriam_datatypes():
         return data
 
 
-# Build Resource Name->Identifier dictionary
-# from web service response
 def build_miriam_identifier_dictionary(miriam_datatype_obj):
+    """ Build dictionary mapping Resource Name->Identifier
+    using data returned from  get_miriam_datatypes(). """
     datatype_dict = {}
-    # Iterate through object and build dictionary
+
     for (root, value_obj) in iteritems(miriam_datatype_obj):
         for k_v_obj in value_obj:
             # Build-up dict with the name, e.g. pir, as Key and
             # Miriam ID as the Value to lookup by the name
-            key = k_v_obj['name'].lower()  #lowercase the name in the dictionary
+            key = k_v_obj['name'].lower()  # lowercase the name in the dictionary
             value = k_v_obj['id']
             datatype_dict[key] = value
     return datatype_dict
 
 
-# Build dictionary of MIRIAM ID / Name 
 def build_miriam_name_dictionary(miriam_datatype_obj):
+    """ Build dictionary mapping MIRIAM ID->Resource Name
+    to use for autocomplete in the form. """
     miriam_name_dict = {}
-    # Iterate through object and build dictionary
+
     for (root, value_obj) in iteritems(miriam_datatype_obj):
         for k_v_obj in value_obj:
             # Build-up dict with the ID as Key and
-            # resource name as the Value 
+            # resource name as the Value
             key = k_v_obj['id']
             value = k_v_obj['name']
             miriam_name_dict[key] = value
     # Sort dict for display in autocomplete
     sorted_datatype_dict = OrderedDict(sorted(miriam_name_dict.items(), key=itemgetter(1)))
-    #print "SDD:", sorted_datatype_dict
+    # print "SDD:", sorted_datatype_dict
     return sorted_datatype_dict
 
 
-# Build MIRIAM ID / Name Autocomplete data structure
-# From this structure, [{'MIR:00000001': 'Wikipedia'}, {'MIR:0000007': 'Some Resource'}]
-# To this e.g. [{value: 'MIR:00000001', label: 'Wikipedia'}, {value: 'MIR:0000007', label: 'Some Resource'}]
 def build_miriam_autocomplete_data(miriam_datatype_obj):
+    """ Build MIRIAM ID / Name Autocomplete data structure
+    From: [{'MIR:00000001': 'Wikipedia'}, {'MIR:0000007': 'Some Resource'}]
+    To: [{value: 'MIR:00000001', label: 'Wikipedia'},
+    {value: 'MIR:0000007', label: 'Some Resource'}]. """
     autocomplete_data = []
-    # NOTE: Autocomplete data needs to be added into children section 
+    # NOTE: Autocomplete data needs to be added into children section
     # [{'text': 'All Resources', 'children': [{}]}]
-    #autocomplete_data_deque = deque()
+    # autocomplete_data_deque = deque()
 
     for (root, value_obj) in iteritems(miriam_datatype_obj):
         for k_v_obj in value_obj:
@@ -81,10 +80,6 @@ def build_miriam_autocomplete_data(miriam_datatype_obj):
 # Main Program
 if __name__ == '__main__':
     miriam_datatype_obj = get_miriam_datatypes()
-    
     miriam_datatype_dict = build_miriam_identifier_dictionary(miriam_datatype_obj)
-
     build_miriam_name_dictionary(miriam_datatype_obj)
-    
     miriam_autocomplete_data = build_miriam_autocomplete_data(miriam_datatype_obj)
-
